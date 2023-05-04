f1e0e8ecb01eaf2fa4da98742981f27de70d2f65
Reggie Schriner <rschriner2@huskers.unl.edu>
2023-05-03 21:01:57 -0500

    Pushing code for later review. Issue where text of all text boxes and labels on the credentials screen will not be updated when the .text attribute is changed. Print statements were added for testing.

--- travel_planner_app/main_travel_planner_app.py
+++ travel_planner_app/main_travel_planner_app.py
@@ -23,55 +23,29 @@ UNITS = 'imperial'
 COUNT = 5
 
 
-class TravelPlannerApp(App):
-    def __init__(self, **kwargs):
-        super(TravelPlannerApp, self).__init__(**kwargs)
-        url = DealsDatabase.construct_mysql_url('localhost', 3306, 'package_deals', 'root', 'cse1208')
-        self.deals_database = DealsDatabase(url)
-        self.session = self.deals_database.create_session()
-
-    def build(self):
-        inspector.create_inspector(Window, self)
-        kv = Builder.load_file("travel_planner.kv")
-        self.rest_connection = rest.RESTConnection(authority='api.openweathermap.org', port=443, root_path='/data/2.5')
-        return kv
-
-    def construct_url(self, get_parameters=None):
-        try:
-            resource = 'forecast'
-            if get_parameters["appid"] != '':
-                url = self.rest_connection.construct_url(resource, get_parameters)
-        except:
-            self.root.ids.apikey.text = ''
-            self.root.ids.error.text = 'Invalid api key, try again.'
-        return url
-    def on_records_loaded(self, _, response):
-        print(dumps(response, indent=4, sort_keys=True))
-        print('success')
-
-    def on_records_not_loaded(self, _, error):
-        self.records = ['[Failed to load records]']
-        print(error)
-
-
-class MainWindow(ScreenManager):
-    pass
-
-
 class CredentialsWindow(Screen):
     def add_credentials(self):
+        app = App.get_running_app()
+        url = app.construct_url({"appid":f"{self.ids.apikey.text}", "units":"imperial"})
+        if url is None:
+            self.ids.message.text = 'Please enter a valid api key'
         attributes = [self.ids.authority.text, self.ids.port.text, self.ids.database.text,
                       self.ids.username.text, self.ids.password.text,
                       self.ids.weatherauthority.text, self.ids.weatherport.text,
                       self.ids.apikey.text]
+        print(attributes)
+        print(self.ids.message.text)
         if any(attribute.isspace() or attribute == '' for attribute in attributes):
+            print('If statement entered')
             self.ids.message.text = 'Fill in all text boxes'
+            print(self.ids.message.text)
             self.ids.authority.text, self.ids.port.text, self.ids.database.text, \
             self.ids.username.text, self.ids.password.text, \
             self.ids.weatherauthority.text, self.ids.weatherport.text, \
             self.ids.apikey.text = '', '', '', '', '', '', '', ''
+            self.canvas.ask_update()
         else:
-            self.ids.message.text = 'Success!'
+            self.ids.error.text = 'Success!'
             addition = combined_installer.Credentials(authority=attributes[0], port=attributes[1],
                                                       database=attributes[2],
                                                       username=attributes[3], password=attributes[4],
@@ -83,6 +57,8 @@ class CredentialsWindow(Screen):
         self.ids.username.text, self.ids.password.text, \
         self.ids.weatherauthority.text, self.ids.weatherport.text, \
         self.ids.apikey.text = '', '', '', '', '', '', '', ''
+        print(url)
+        return url
 
 
 class LoadingScreen(Screen):
@@ -105,9 +81,40 @@ class ValidateLocationsPage(Screen):
 class UpdateRatingsPage(Screen):
     pass
 
+class WindowManager(ScreenManager):
+    pass
 
 class ItineraryPage(Screen):
     pass
+
+class TravelPlannerApp(App):
+    def __init__(self, **kwargs):
+        super(TravelPlannerApp, self).__init__(**kwargs)
+        url = DealsDatabase.construct_mysql_url('localhost', 3306, 'package_deals', 'root', 'cse1208')
+        self.deals_database = DealsDatabase(url)
+        self.session = self.deals_database.create_session()
+
+    def build(self):
+        inspector.create_inspector(Window, self)
+        kv = Builder.load_file("travel_planner.kv")
+        self.credentials_window = CredentialsWindow()
+        self.rest_connection = rest.RESTConnection(authority='api.openweathermap.org', port=443, root_path='/data/2.5')
+        return kv
+
+    def construct_url(self, get_parameters=None):
+        resource = 'forecast'
+        if get_parameters["appid"] != '':
+            url = self.rest_connection.construct_url(resource, get_parameters)
+            return url
+
+
+    def on_records_loaded(self, _, response):
+        print(dumps(response, indent=4, sort_keys=True))
+        print('success')
+
+    def on_records_not_loaded(self, _, error):
+        self.records = ['[Failed to load records]']
+        print(error)
 
 
 if __name__ == '__main__':
