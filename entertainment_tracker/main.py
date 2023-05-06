4a04dd88689f1484e97c93b26965011ccc714405
Phoenix Fletcher <phoenixfletcher@Phoenixs-MBP-2.home>
2023-05-06 15:42:15 -0500

    added incolmplete implementation of entertainment tracker.

--- entertainment_tracker/main.py
+++ entertainment_tracker/main.py
@@ -1,7 +1,10 @@
 from kivy.app import App
 from kivy.properties import BooleanProperty, StringProperty
+from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
+from entertainment import EntertainmentDatabase, City, Venue, WeatherCondition
+import mysql.connector
 
 Window.size = (400, 650)
 
@@ -11,40 +14,64 @@ class HomeScreen(Screen):
 
 
 class CityScreen(Screen):
-    name = ''
-    country = ''
-    lat = ''
-    long = ''
+    open_popup = True
+    name = StringProperty('')
 
     def save_city_inputs(self):
+        app = App.get_running_app()
         name = self.ids.name.text
+        self.name = name
         country = self.ids.country.text
         lat = self.ids.lat.text
         long = self.ids.long.text
 
-        if len(name) > 0:
+        if any(attribute.isspace() or attribute == '' for attribute in [name, country, lat, long]):
+            self.ids.error.text = 'Please fill in all the blanks'
+            self.open_popup = False
+        else:
+            self.ids.error.text = ''
+            self.open_popup = True
+            lat = int(float(lat) * 10000)
+            long = int(float(long) * 10000)
             self.name = name
-        if len(country) > 0:
-            self.country = country
-        if len(lat) > 0:
-            self.lat = lat
-        if len(long) > 0:
-            self.long = long
+            # addition = City(name=name, country=country, lat=int(lat), long=int(long))
+            # app.commit(addition)
+
+    def show_popup(self):
+        popup = CityPopup(self)
+        popup.open()
+
+
+class CityPopup(Popup):
+    def __init__(self, obj, **kwargs):
+        super(CityPopup, self).__init__(**kwargs)
+        self.obj = obj
+
+
+venue = ''
 
 
 class VenueScreen(Screen):
-    venue_name = ''
-    city = ''
-    type = ''
+    open_weather_screen = True
 
     def save_venue_inputs(self):
-        self.venue_name = self.ids.venue_name.text
-        self.city = self.ids.city.text
-        self.type = self.ids.type.text
-        print(self.city + self.venue_name)
+        app = App.get_running_app()
+        venue_name = self.ids.venue_name.text
+        city = self.ids.city.text
+        venue_type = self.ids.type.text
+
+        if any(attribute.isspace() or attribute == '' for attribute in [venue_name, city, type]):
+            self.ids.error.text = 'Please fill in all of the blanks.'
+            self.open_weather_screen = False
+        else:
+            open_weather_screen = True
+            self.ids.error.text = ''
+            # addition = Venue(name=venue_name, city=city, type=type)
+            # app.commit(addition)
 
 
 class WeatherScreen(Screen):
+    venue = None
     thunderstorm = ''
     drizzle = ''
     rain = ''
@@ -85,6 +112,22 @@ class WeatherScreen(Screen):
         else:
             self.wind_speed = None
 
+        if self.thunderstorm == 'down':
+            # addition = WeatherCondition(condition_code=2, venue=)
+            pass
+        elif self.drizzle == 'down':
+            pass
+        elif self.rain == 'down':
+            pass
+        elif self.snow == 'down':
+            pass
+        elif self.atmosphere == 'down':
+            pass
+        elif self.clear == 'down':
+            pass
+        elif self.clouds == 'down':
+            pass
+
 
 class WindowManager(ScreenManager):
     pass
@@ -92,6 +135,16 @@ class WindowManager(ScreenManager):
 
 class EntertainmentApp(App):
     advancing = BooleanProperty(True)
+
+    def __init__(self, **kwargs):
+        super(EntertainmentApp, self).__init__(**kwargs)
+        url = EntertainmentDatabase.construct_mysql_url('localhost', 3306, 'package_deals', 'root', 'cse1208')
+        self.entertainment_database = EntertainmentDatabase(url)
+        self.session = self.entertainment_database.create_session()
+
+    def commit_to_db(self, addition):
+        self.session.add(addition)
+        self.session.commit()
 
 
 if __name__ == '__main__':
